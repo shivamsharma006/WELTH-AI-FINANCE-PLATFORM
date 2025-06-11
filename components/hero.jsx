fdad28621c2840178343060240d4509fb7a53bf6
SHIVAM SHARMA <147228448+shivamsharma006@users.noreply.github.com>
2025-06-11 23:52:45 +0530

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -42,9 +42,9 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="https://github.com/shivamsharma006">
             <Button size="lg" variant="outline" className="px-8">
-              Watch Demo
+               GitHub
             </Button>
           </Link>
         </div>
